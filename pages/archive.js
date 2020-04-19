a57710eb5959af3ed3f21acdd31fb5e5279c73e8
Paul Miller <paul@pauljmiller.com>
2020-04-19 12:53:03 -0400

    april 19th

--- pages/archive.js
+++ pages/archive.js
@@ -6,6 +6,13 @@ import SermonItem from "../components/SermonItem";
 const Home = () => (
   <Layout>
     <SermonItem
+      date="April 12th"
+      title="1 Timothy 3, The Mystery of Godliness, He is alive"
+      embedId="rve5UVcdniY"
+      sermonPdf="/spiritword_sermon_4-12-20.pdf"
+      songsPdf="/spiritword_songs_4-12-20.pdf"
+    />
+    <SermonItem
       date="April 5th"
       title="1 Timothy 3:6-7, Instruction for Elders and Deacons"
       embedId="aelYPTVFig4"
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ const Home = () => (
     <p className="description">Virtual church starts here at 10:30AM PDT!</p>
 
     <SermonItem
-      date="April 12th"
-      title="1 Timothy 3, The Mystery of Godliness, He is alive"
-      embedId="rve5UVcdniY"
-      sermonPdf="/spiritword_sermon_4-12-20.pdf"
-      songsPdf="/spiritword_songs_4-12-20.pdf"
+      date="April 19th"
+      title="1 Timothy 4, Controlling Our Behavior"
+      embedId="CI_uK8ZoZNo"
+      sermonPdf="/spiritword_sermon_4-19-20.pdf"
+      songsPdf="/spiritword_songs_4-19-20.pdf"
     />
 
     <p>

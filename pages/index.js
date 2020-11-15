e848b4c071fed3ba6d11b3c202a55e001e9227e2
Paul Miller <paul@pauljmiller.com>
2020-11-15 13:21:08 -0500

    nov 15 update

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ const Home = () => (
     <p className="description">Virtual church starts here at 10:30AM PDT!</p>
 
     <SermonItem
-      date="November 8"
-      title="Titus 1:5-10, Appointing Qualified Elders"
-      embedId="aBQdAs8RCfk"
-      sermonPdf="/spiritword_sermon_11-8-20.pdf"
-      songsPdf="/spiritword_songs_11-8-20.pdf"
+      date="November 15"
+      title="Titus 1:5-11, Appointing Qualified Elders"
+      embedId="CPnPVNYrx8o"
+      sermonPdf="/spiritword_sermon_11-15-20.pdf"
+      songsPdf="/spiritword_songs_11-15-20.pdf"
     />
 
     <p>

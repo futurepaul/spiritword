726c5ac84dd360dd611635989f645aa6b4523b0e
Paul Miller <paul@pauljmiller.com>
2021-05-02 13:17:04 -0400

    typescript grr

--- pages/new.tsx
+++ pages/new.tsx
@@ -5,9 +5,78 @@ import { supabase } from "../lib/initSupabase";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 
+/**
+ * Better DateTimeFormatOptions types
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat for representation details
+ */
+export interface DateTimeFormatOptions extends Intl.DateTimeFormatOptions {
+  localeMatcher?: "best fit" | "lookup";
+  weekday?: "long" | "short" | "narrow";
+  era?: "long" | "short" | "narrow";
+  year?: "numeric" | "2-digit";
+  month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
+  day?: "numeric" | "2-digit";
+  hour?: "numeric" | "2-digit";
+  minute?: "numeric" | "2-digit";
+  second?: "numeric" | "2-digit";
+  timeZoneName?: "long" | "short";
+  formatMatcher?: "best fit" | "basic";
+  hour12?: boolean;
+  /**
+   * Timezone string must be one of IANA. UTC is a universally required recognizable value
+   */
+  timeZone?: "UTC" | string;
+  dateStyle?: "full" | "long" | "medium" | "short";
+  timeStyle?: "full" | "long" | "medium" | "short";
+  calendar?:
+    | "buddhist"
+    | "chinese"
+    | " coptic"
+    | "ethiopia"
+    | "ethiopic"
+    | "gregory"
+    | " hebrew"
+    | "indian"
+    | "islamic"
+    | "iso8601"
+    | " japanese"
+    | "persian"
+    | "roc";
+  dayPeriod?: "narrow" | "short" | "long";
+  numberingSystem?:
+    | "arab"
+    | "arabext"
+    | "bali"
+    | "beng"
+    | "deva"
+    | "fullwide"
+    | " gujr"
+    | "guru"
+    | "hanidec"
+    | "khmr"
+    | " knda"
+    | "laoo"
+    | "latn"
+    | "limb"
+    | "mlym"
+    | " mong"
+    | "mymr"
+    | "orya"
+    | "tamldec"
+    | " telu"
+    | "thai"
+    | "tibt";
+  hourCycle?: "h11" | "h12" | "h23" | "h24";
+  /**
+   * Warning! Partial support
+   */
+  fractionalSecondDigits?: 0 | 1 | 2 | 3;
+}
+
 function toHtmlDate(date: Date): string {
   // May 19, 2021
-  const options = {
+  const options: DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
